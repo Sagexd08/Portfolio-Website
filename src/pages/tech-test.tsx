fda867cbf4515e797225b482b873cdfd3994f94c
Sohom Chatterjee <sohomchatterjee07@gmail.com>
2025-05-09 01:50:40 +0530

    Added Technologies section to the portfolio website

---
+++ src/pages/tech-test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Technologies from '@/components/Technologies.simple';
+
+const TechTest: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <Technologies />
+    </div>
+  );
+};
+
+export default TechTest;
